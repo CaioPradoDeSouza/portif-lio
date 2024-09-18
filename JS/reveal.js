9ca79443359c9154cd96f7a3077811e03812895b
Caio Prado de Souza <cpssouza7@gmail.com>
2024-09-17 21:57:54 -0300

    create scrollReveal

---
+++ JS/reveal.js
@@ -0,0 +1,49 @@
+window.revelar = ScrollReveal({ reset: true })
+
+revelar.reveal('.efeito-txt-topo',
+    {
+        duration: 2500,
+        delay: 500,
+        distance: '90px',
+    })
+
+revelar.reveal('.efeito-ferramentas',
+    {
+        duration: 2000,
+        delay: 1000,
+        distance: '90px',
+    })
+
+revelar.reveal('.titulo-aprendizados',
+    {
+        duration: 2000,
+        delay: 500,
+        distance: '90px',
+    })
+
+    
+revelar.reveal('.conteudo-aprendizados',
+    {
+        duration: 2000,
+        delay: 1000,
+        distance: '90px',
+    })
+
+        
+revelar.reveal('.efeito-img-sobre',
+    {
+        
+        duration: 1500,
+        delay: 500,
+        distance: '90px',
+        origin: 'left',
+    })
+
+    revelar.reveal('.efeito-txt-sobre',
+        {
+            
+            duration: 2000,
+            delay: 1000,
+            distance: '90px',
+            origin: 'right',
+        })
